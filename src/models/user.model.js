55f750274df4d60f8d49ae0b91ae4b35f8e1eb14
yash Bhoir <yash51217@gmail.com>
2024-06-03 17:17:57 +0530

    model and jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose ,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema ({
+
+    username : {
+        type : String,
+        required : true ,
+        unique: true,
+        lowercase: true,
+        trim: true ,
+        index: true
+    },
+    email : {
+        type : String,
+        required : true ,
+        unique: true,
+        lowercase: true,
+        trim: true ,
+    },
+     fullname  : {
+        type : String,
+        required : true ,
+        trim: true ,
+        index: true
+    },
+    avatar : {
+        type : String,
+        required : true ,
+    },
+    coverImage : {
+        type : String,
+    },
+    watchHistory :[ {
+        type : Schema.Types.ObjectId,
+        ref: "video"
+    }],
+    password: {
+        type :String,
+        required : [true , 'password is required ']
+    },
+    refreshToken : {
+        type : String 
+    }
+
+},
+{
+    timestamps : true
+}
+)
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+})
+
+userSchema.method.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+export const  User = mongoose.model("User", userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        view: {
+            type: Number,
+            default: 0
+        },
+        isPublish: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+
+        }
+    },
+    {
+        timestamps: true
+    }
+
+)
+
+export const Video = mongoose.model("Video", videoSchema)
